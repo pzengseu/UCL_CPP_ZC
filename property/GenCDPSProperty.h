9c009e62190cb4da22fd7db82335c4b8a15cee3e
zpeng <zpeng@seu.edu.cn>
2017-07-30 20:50:58 +0800

    UCL标准属性生成方法修改

--- property/GenCDPSProperty.h
+++ property/GenCDPSProperty.h
@@ -8,13 +8,34 @@
 #include "UCLPropertyBase.h"
 
 /**
- * CDPS, title, originality, file Description
+ * CDPS, title, keywords, abstract, author, entity, Tag, copyright,  originality, file Description
  */
 class GenCDPSProperty
 {
 public:
     //title
     static UCLPropertyBase genTitle(string vPart, uint8_t helper=0);
+
+    //keywords
+    static UCLPropertyBase genKeywords(uint8_t count, string vPart, uint8_t helper=0);
+
+    //abstract
+    static UCLPropertyBase genAbstract(string vPart, uint8_t helper=0);
+
+    //author
+    static UCLPropertyBase genAuthor(uint8_t persons, uint8_t companies, string vPart, uint8_t helper=0);
+
+    //entity
+    static UCLPropertyBase genEntity(uint8_t count, string vPart, uint8_t helper=0);
+
+    //Tag
+    static UCLPropertyBase genTag(uint8_t count, string vPart, uint8_t helper=0);
+
+    //copyright
+    static UCLPropertyBase genCopyright(string vPart, uint8_t helper=0);
+
+    //originality
+    static UCLPropertyBase genOriginality(string vPart, uint8_t helper=0);
 
     //file description
     static UCLPropertyBase genFileDescription(string vPart, uint8_t helper=0);
